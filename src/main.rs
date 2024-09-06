a4afc93b11e3534a785fb73d6d98ea896e68450a
NXVZBGBFBEN <nxvzbgbfben@gmail.com>
2024-09-07 06:12:38 +0900

    package download OK

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use ketcindyinstaller::argument_parser;
 use ketcindyinstaller::argument_parser::Subcommand;
 use ketcindyinstaller::package_manager::download_package;
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let project_directories = directories::ProjectDirs::from("", "NXVZBGBFBEN", "KeTCindyInstaller")
+        .context("No valid home directory path could be retrieved from the operating system.")?;
+
     let argument = argument_parser::parse_argument();
     match argument.subcommand {
         Subcommand::Install { nodeps: _, packages } => {
             for package in packages {
-                let manifest = download_package(package.to_string()).await?;
-                println!("{:?}", manifest);
+                let download_location = project_directories.cache_dir().join("packages").join(package.to_string());
+                download_package(package.to_string(), &download_location).await?;
             }
         }
     }
--- src/package_manager.rs
+++ src/package_manager.rs
@@ -1,6 +1,9 @@
-use std::time::Duration;
-use anyhow::Result;
+use std::{io::Write, time::Duration};
+use std::path::Path;
+use std::fs;
+use anyhow::{Context, Result};
 use serde::Deserialize;
+use indicatif::ProgressBar;
 
 #[derive(Deserialize, Debug)]
 pub struct Manifest {
@@ -14,14 +17,30 @@ pub struct Package {
     pub url: String
 }
 
-pub async fn download_package(package_name: String) -> Result<()> {
+pub async fn download_package(package_name: String, download_location: &Path) -> Result<()> {
     let manifest = fetch_manifest(package_name).await?;
+    let package_file_location = download_location.join(url::Url::parse(&manifest.package.url)?
+        .path_segments().context("Failed to parse package URL.")?
+        .last().context("Failed to parse package URL.")?);
+    let spinner = ProgressBar::new_spinner().with_message("Downloading package...");
+
+    spinner.enable_steady_tick(Duration::from_millis(75));
+
+    let package = reqwest::get(&manifest.package.url)
+        .await?
+        .error_for_status()?
+        .bytes()
+        .await?;
+    fs::create_dir_all(download_location)?;
+    fs::File::create(package_file_location)?.write_all(&package)?;
+
+    spinner.finish();
 
     Ok(())
 }
 
 async fn fetch_manifest(package_name: String) -> Result<Manifest> {
-    let spinner = indicatif::ProgressBar::new_spinner().with_message("Fetching manifest...");
+    let spinner = ProgressBar::new_spinner().with_message("Fetching manifest...");
 
     spinner.enable_steady_tick(Duration::from_millis(75));
 
