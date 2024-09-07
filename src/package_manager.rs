7a31f838c76ca0787f439deaa5b541b3f818a765
NXVZBGBFBEN <nxvzbgbfben@gmail.com>
2024-09-08 07:39:50 +0900

    update Manifest

--- src/package_manager.rs
+++ src/package_manager.rs
@@ -6,21 +6,38 @@ use std::path::Path;
 use std::{io::Write, time::Duration};
 
 #[derive(Deserialize, Debug)]
-pub struct Manifest {
-    pub package: Package,
+struct Manifest {
+    package: Package,
 }
 
 #[derive(Deserialize, Debug)]
-pub struct Package {
-    pub name: String,
-    pub version: String,
-    pub url: String,
+struct Package {
+    #[allow(dead_code)]
+    name: String,
+    #[allow(dead_code)]
+    version: String,
+    target: PackageTarget,
+}
+
+#[derive(Deserialize, Debug)]
+struct PackageTarget {
+    universal: Option<PackageMetadata>,
+}
+
+#[derive(Deserialize, Debug)]
+struct PackageMetadata {
+    url: String,
 }
 
 pub async fn download_package(package_name: String, download_location: &Path) -> Result<()> {
     let manifest = fetch_manifest(package_name).await?;
+    let package_metadata = if let Some(metadata) = manifest.package.target.universal {
+        metadata
+    } else {
+        todo!("parse metadata (OS, arch)")
+    };
     let package_file_location = download_location.join(
-        url::Url::parse(&manifest.package.url)?
+        url::Url::parse(&package_metadata.url)?
             .path_segments()
             .context("Failed to parse package URL.")?
             .last()
@@ -30,7 +47,7 @@ pub async fn download_package(package_name: String, download_location: &Path) ->
 
     spinner.enable_steady_tick(Duration::from_millis(75));
 
-    let package = reqwest::get(&manifest.package.url)
+    let package = reqwest::get(&package_metadata.url)
         .await?
         .error_for_status()?
         .bytes()
